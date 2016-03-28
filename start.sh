10236895ebff1b7f1385980a4f1f90159c5c6f58
Yoojia Chen <yoojiachen@gmail.com>
2016-03-28 16:57:33 +0800

    UPdate start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 SAVE_NAME="ourworld"
+SAVE_MAP="${SAVE_NAME}.zip"
+
 AUTOSAVE_INTERVAL="--autosave-interval 30"
 AUTOSAVE_SLOTS="--autosave-slots 3"
 DISABLED_COMMANDS="--disallow-commands"
@@ -9,13 +11,25 @@ EXEC_DIR="/factorio"
 EXEC_BIN="${EXEC_DIR}/bin/x64/factorio"
 EXEC_ARGS="${DISABLED_COMMANDS} ${AUTOSAVE_INTERVAL} ${AUTOSAVE_SLOTS}"
 
+AUTOSAVE_DIR="${EXEC_DIR}/saves"
+
 echo "# Factorio Server launcher start..."
-# Check save map
-if [ ! -f ${EXEC_DIR}/saves/${SAVE_NAME}.zip ]; then
-    echo " - Create a new map: ${SAVE_NAME}"
-    ${EXEC_BIN} --create ${SAVE_NAME}
+
+# find the latest autosaveN.zip
+LATEST_MAP=`ls -t ${AUTOSAVE_DIR}/*.zip 2> /dev/null | head -1`
+
+echo "# Latest autosave map: ${LATEST_MAP}"
+
+if [ "${LATEST_MAP}" == "${AUTOSAVE_DIR}/${SAVE_MAP}" ]; then
+    echo "- Using origin map: ${LATEST_MAP}"
 else
-    echo " - Using existting map: ${SAVE_NAME}"
+    echo "- Using latest map: ${LATEST_MAP}"
+    mv ${LATEST_MAP} ${AUTOSAVE_DIR}/${SAVE_MAP}
+fi
+
+# Check save map
+if [ ! -f ${LATEST_MAP} ]; then
+    ${EXEC_BIN} --create ${SAVE_NAME}
 fi
 
 # Run server with args
